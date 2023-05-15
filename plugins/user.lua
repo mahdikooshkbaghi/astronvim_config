7b5d0148f1d1807faf73a929d927548896aba5c4
Mahdi Kooshkbaghi <mahdikooshkbaghi@users.noreply.github.com>
2023-05-15 12:46:54 -0400

    Add files via upload

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,15 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+ {
+  -- For better integration install ripgrep
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {},
+  event = "User AstroFile",
+  cmd = { "TodoQuickFix" },
+  keys = {
+   { "<leader>T", "<cmd>TodoTelescope<cr>"}
+  }
+ },
 }
